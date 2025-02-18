f38c5ffb2c806d828e0c1de109353d1daf85f0f0
Luma Pires <luma.pires@opwell.ai>
2025-02-18 16:34:38 -0300

    Insert function to paint pixel when element is clicked

--- javascript.js
+++ javascript.js
@@ -5,22 +5,23 @@ const size = document.querySelector("#size");
 const containerHeight = container.offsetHeight;
 const containerWidth = container.offsetWidth;
 
-// Events
+// HomePage
 
-size.addEventListener("keydown", insertingNewContainerPixels)
-
-for (const pixel of container.children) {
-    pixel.addEventListener("click", coloringPixel(pixel, True))
-    pixel.addEventListener("contextmenu", coloringPixel(pixel, False))
+function openingPage() {
+    document.querySelector("#size").value = 10
+    insertingNewContainerPixels()
 }
 
 // Coloring
 
 function coloringPixel(element, active){
     if (active) {
-        element.classList.add("active")
+        element.classList.add("active");
+        element.classList.remove("inactive");
     } else {
-        element.classList.add("inactive")}
+        element.classList.add("inactive");
+        element.classList.remove("inactive");
+    }
 }
 
 // Pixels in container
@@ -30,11 +31,9 @@ function insertingPixels() {
     const widthPerPixel = containerWidth/nPixels;
     const heightPerPixel = containerHeight/nPixels;
     const fragment = document.createDocumentFragment();
-    console.log(nPixels)
     for (i=0; i<nPixels**2; i++){
-        console.log(i)
         let pixel = document.createElement("div");
-        pixel.classList.add("pixel")
+        pixel.classList.add("pixel");
         pixel.setAttribute("style", `width: ${widthPerPixel}px; height: ${heightPerPixel}px;`);
         fragment.appendChild(pixel);
     }
@@ -42,21 +41,37 @@ function insertingPixels() {
 }
 
 function deletingPixels() {
-    container.innerHTML = ""
+    container.innerHTML = "";
 }
 
 // New Pixel size
 
 function insertingNewContainerPixels() {
-    deletingPixels()
-    insertingPixels()
+    deletingPixels();
+    insertingPixels();
+    addingEventsToNewPixels();
 }
 
 // Reset
 
  function resetingGame() {
-    let pixels = container.children
+    let pixels = container.children;
     for (const pixel of pixels){
-        coloringPixel(pixel, False)
+        coloringPixel(pixel, False);
     }
  }
+
+ function addingEventsToNewPixels() {
+    const pixels = Array.from(container.children);
+    pixels.forEach(pixel => {
+        pixel.addEventListener("click", () => coloringPixel(pixel, true))
+        pixel.addEventListener("contextmenu", () => coloringPixel(pixel, false))
+    });
+ }
+
+// Events
+
+document.addEventListener("DOMContentLoaded", () => openingPage())
+
+size.addEventListener("keydown", insertingNewContainerPixels)
+
